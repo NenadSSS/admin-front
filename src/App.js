ae8a3173c16694633859eba5e78ea36b413fdad6
nenadSavovic <nenad@glimpse.me>
2020-06-27 12:48:09 +0200

    Add styling with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row } from "react-bootstrap";
+
 import { UserContainer } from "./containers/UserContainer";
+import { Login } from "./components/login";
 
 function App() {
   const client = new ApolloClient({
     uri: "http://localhost:8080/v1/graphql",
   });
+  const customHistory = createBrowserHistory();
 
   return (
     <div className="App">
       <ApolloProvider client={client}>
-        <main>Admin app</main>
-        <UserContainer />
+        <Router history={customHistory}>
+          <h4>Admin app</h4>
+          <br />
+          <Switch>
+            <Route path="/">
+              <Container>
+                <Row className="justify-content-md-center">
+                  <Login />
+                </Row>
+              </Container>
+            </Route>
+          </Switch>
+          {/* <UserContainer /> */}
+        </Router>
       </ApolloProvider>
     </div>
   );
